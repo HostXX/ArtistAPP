1f2b1f2f7c30eada5b901f2963074f330b352059
Carlos Garcia <carlosmauriciogarcia13@gmail.com>
2020-09-24 23:44:50 -0400

    changed router to hashrouter

--- build/precache-manifest.f5dbe73054673f3ad7150b0a7544c5d2.js
+++ build/precache-manifest.f5dbe73054673f3ad7150b0a7544c5d2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4c9ae29988e1c6f730fea309090a3039",
+    "revision": "188086a5132c1c205711c4e0d4b22ec1",
     "url": "/index.html"
   },
   {
-    "revision": "3d720dbfffdbf077a814",
+    "revision": "d6cab8acf41a3ea7f3f5",
     "url": "/static/css/main.d9210b0e.chunk.css"
   },
   {
-    "revision": "61213f32d9fb763dbe02",
-    "url": "/static/js/2.69d5540e.chunk.js"
+    "revision": "809fef3d5c0a88e0bf71",
+    "url": "/static/js/2.91fcbe48.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.69d5540e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.91fcbe48.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d720dbfffdbf077a814",
-    "url": "/static/js/main.691cc5ff.chunk.js"
+    "revision": "d6cab8acf41a3ea7f3f5",
+    "url": "/static/js/main.f398df85.chunk.js"
   },
   {
     "revision": "54458ba7428c23de5d8a",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ada9ff835aa84a8a0173b73e1ab138d.js"
+  "/precache-manifest.f5dbe73054673f3ad7150b0a7544c5d2.js"
 );
 
 self.addEventListener('message', (event) => {
